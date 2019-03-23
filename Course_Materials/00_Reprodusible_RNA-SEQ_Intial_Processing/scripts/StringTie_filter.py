ccfbf52c64f6b2955bb5cdcbfeafc6623a28114e
geparada <geparada88@gmail.com>
2019-03-23 18:51:55 +0000

    Update StringTie_filter.py

--- Course_Materials/00_Reprodusible_RNA-SEQ_Intial_Processing/scripts/StringTie_filter.py
+++ Course_Materials/00_Reprodusible_RNA-SEQ_Intial_Processing/scripts/StringTie_filter.py
@@ -29,10 +29,28 @@ def main(gffcompare_annotated_gtf, gffcompare_tracking, exon_number_filter, clas
         if tag=="transcript_id":
           transcript_id=value
         else:
-          tag_dict[transcript_id][tag] = value
+          tag_dict[transcript_id][tag] = value   #exon number should overwrite, so the final dict will have the total number of exons
         
-  print(tag_dict)
+  
+  # All the data is collected at dictionaries at this point
+  
+  
+  for row in csv.reader(open(gffcompare_annotated_gtf), delimiter="\t"):
 
+    
+    for tag in row[-1].split(";"):
+      tag_value = tag.strip(" ").split(" ")    
+      
+      if len(tag_value)==2:
+        tag, value = tag_value
+ 
+        if tag=="transcript_id":
+          transcript_id=value
+      
+    exon_number = tag_dict[transcript_id]["exon_number"]
+    stringtie_class = transcript_class_dict[transcript_id]
+    
+    print(stringtie_class, exon_number)
       
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4])
