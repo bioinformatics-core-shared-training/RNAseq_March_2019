14dd8f7a7257ef5337f5eb69fd8922b64cbab9cc
geparada <geparada88@gmail.com>
2019-03-23 18:01:54 +0000

    Update StringTie_filter.py

--- Course_Materials/00_Reprodusible_RNA-SEQ_Intial_Processing/scripts/StringTie_filter.py
+++ Course_Materials/00_Reprodusible_RNA-SEQ_Intial_Processing/scripts/StringTie_filter.py
@@ -3,15 +3,22 @@ import csv
 
 def main(gffcompare_annotated_gtf, gffcompare_tracking ):
 
+  transcript_class_dict = {}
+  
   for row in csv.reader(open(gffcompare_tracking), delimiter="\t"):
     
     stringtie_class = row[3]
     transcript = row[4].split("|")[1]
 
-    print(transcript, stringtie_class)
+    transcript_class_dict[transcript] = stringtie_class
+
   
-#  for row in csv.reader(open(gffcompare_annotated_gtf), delimiter="\t"):
-#    print(row)
+  for row in csv.reader(open(gffcompare_annotated_gtf), delimiter="\t"):
+    
+    print(row)
+    
+    for tag in row[-1].split(";"):
+      print(tag)
   
   
 
