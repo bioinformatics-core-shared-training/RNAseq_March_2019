21d4dc88dc7bbaaff23d3ff95097ff32e34dd7f9
geparada <geparada88@gmail.com>
2019-03-24 21:50:26 +0000

    change name

---
+++ Course_Materials/00_Reproducible_RNA-Seq_Processing/download/SRR7889581.download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+srr="SRR7889581"
+fastq-dump.2.9.1 --split-files -O FASTQ  SRR7889581
+numLines=$(fastq-dump.2.9.1 -X 1 -Z --split-spot $srr | wc -l)
+if [ $numLines -eq 8 ]; then cat FASTQ/${srr}_1.fastq FASTQ/${srr}_2.fastq > FASTQ/$srr.fastq && rm FASTQ/${srr}_1.fastq FASTQ/${srr}_2.fastq; fi
+if [ -f FASTQ/${srr}_1.fastq ]; then mv FASTQ/${srr}_1.fastq FASTQ/${srr}.fastq ; elif [ -f FASTQ/${srr}_2.fastq ]; then mv FASTQ/${srr}_2.fastq FASTQ/${srr}.fastq; fi
+gzip FASTQ/${srr}.fastq
---
+++ Course_Materials/00_Reproducible_RNA-Seq_Processing/download/genome.download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.fa.gz -O Genome/dm6.fa.gz
+gzip -d Genome/dm6.fa.gz
